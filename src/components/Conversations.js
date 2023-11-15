1faf468953ccc8636f29dc9090a43fdd2aa2f8da
sksabbirhossain <sksabbirhossain4@gmail.com>
2023-11-15 20:46:08 +0600

    setup redux store

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -4,27 +4,31 @@ import { ConversationItem } from "./ConversationItem";
 
 export const Conversations = () => {
   return (
-    <div className="sm:w-[375px] h-full min-h-screen fixed">
+    <div className="sm:w-[375px] h-[84vh] fixed">
+      <div>
+        <ConversationHeader />
+      </div>
+      <div className="bg-white w-full h-full rounded-md mt-1 relative overflow-y-auto">
+        <p className=" px-3 py-1 text-xs font-medium text-gray-600">ALL</p>
         <div>
-          <ConversationHeader />
-        </div>
-        <div className="bg-white h-full min-h-screen rounded-md mt-1 relative">
-          <p className=" px-3 py-1 text-xs font-medium text-gray-600">ALL</p>
-          <div>
-            <ConversationItem />
-            <ConversationItem />
-            <ConversationItem />
-            <ConversationItem />
-            <ConversationItem />
-            <ConversationItem />
-          </div>
-        </div>
-        {/* add conversations */}
-        <div className="absolute bottom-10 right-5">
-          <button className="bg-orange-600 hover:bg-orange-700 text-white duration-150 px-4 py-2 rounded-full text-2xl">
-            +
-          </button>
+          <ConversationItem />
+          <ConversationItem />
+          <ConversationItem />
+          <ConversationItem />
+          <ConversationItem />
+          <ConversationItem />
+          <ConversationItem />
+          <ConversationItem />
+          <ConversationItem />
+          <ConversationItem />
         </div>
       </div>
+      {/* add conversations */}
+      <div className="absolute bottom-0 right-5">
+        <button className="bg-orange-600 hover:bg-orange-700 text-white duration-150 px-4 py-2 rounded-full text-2xl">
+          +
+        </button>
+      </div>
+    </div>
   );
 };
--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -5,7 +5,7 @@ import { Conversations } from "../components/Conversations";
 export const Main = () => {
   return (
     <div className="bg-gray-300 w-full h-screen">
-      <div className="container mx-auto  py-5">
+      <div className="container mx-auto py-5">
         <Conversations />
         <Outlet />
       </div>
