b78fc34cb515f7bd5ba758abc7c2af8417da8ba8
sksabbirhossain <sksabbirhossain4@gmail.com>
2023-11-15 21:29:49 +0600

    created user login mutation and save userInfo in localstroage and redux store

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -10,7 +10,36 @@ export const authApi = apiSlice.injectEndpoints({
         body: data,
       }),
     }),
+
+    // user login mutation
+    login: builder.mutation({
+      query: (data) => ({
+        url: "/user/login",
+        method: "POST",
+        body: data,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const { data } = await queryFulfilled;
+          // save userInfo in localstorage
+          localStorage.setItem(
+            "auth",
+            JSON.stringify({
+              accessToken: data.accessToken,
+              user: data.user,
+            })
+          );
+          // save userInfo in local redux localStore
+          dispatch(
+            userLoggedIn({
+              accessToken: data.accessToken,
+              user: data.user,
+            })
+          );
+        } catch (err) {}
+      },
+    }),
   }),
 });
 
-export const { useRegisterMutation } = authApi;
+export const { useRegisterMutation, useLoginMutation } = authApi;
